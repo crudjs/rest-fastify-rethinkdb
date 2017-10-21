3cddab5608cad00481c109a69922541d480c3610
David Guijarro <guijarro.dav@gmail.com>
2017-10-21 15:29:36 +0200

    Basic walking skeleton

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const fastify = require('fastify')();
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' });
+});
+
+// Run the server!
+fastify.listen(process.env.PORT || 3000, err => {
+  if (err) throw err;
+  console.log(`server listening on ${fastify.server.address().port}`);
+});
