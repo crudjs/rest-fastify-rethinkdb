4990779bf7b0a9ebde7a3d809d8ec18f1e20865a
David Guijarro <guijarro.dav@gmail.com>
2017-10-21 15:32:33 +0200

    ESLint

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
-const fastify = require('fastify')();
+const fastify = require('fastify')()
 
 // Declare a route
 fastify.get('/', (request, reply) => {
-  reply.send({ hello: 'world' });
-});
+  reply.send({ hello: 'world' })
+})
 
 // Run the server!
 fastify.listen(process.env.PORT || 3000, err => {
-  if (err) throw err;
-  console.log(`server listening on ${fastify.server.address().port}`);
-});
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
